bcb2dd49e2764808f5913d8e258afbdd89430662
Jon Wurtzler <jon.wurtzler@gmail.com>
2015-12-10 14:24:22 -0600

    Updated var naming

--- tests/WrappingNeedsTest.php
+++ tests/WrappingNeedsTest.php
@@ -8,41 +8,41 @@ class WrappingNeedsTest extends PHPUnit_Framework_TestCase
   // TC comes from the website examples. (TC = Test Case)
   public function testWrappingNeedsTC1()
   {
-    $WrappingNeeds = new WrappingNeeds();
-    $this->assertEquals(58, $WrappingNeeds->wrappingNeeded("2x3x4"));
+    $wrappingNeeds = new WrappingNeeds();
+    $this->assertEquals(58, $wrappingNeeds->wrappingNeeded("2x3x4"));
   }
 
   public function testWrappingNeedsTC2()
   {
-    $WrappingNeeds = new WrappingNeeds();
-    $this->assertEquals(43, $WrappingNeeds->wrappingNeeded("1x1x10"));
+    $wrappingNeeds = new WrappingNeeds();
+    $this->assertEquals(43, $wrappingNeeds->wrappingNeeded("1x1x10"));
   }
 
   public function testRibbonTC1()
   {
-    $WrappingNeeds = new WrappingNeeds();
-    $this->assertEquals(34, $WrappingNeeds->ribbonNeeded("2x3x4"));
+    $wrappingNeeds = new WrappingNeeds();
+    $this->assertEquals(34, $wrappingNeeds->ribbonNeeded("2x3x4"));
   }
 
   public function testRibbonTC2()
   {
-    $WrappingNeeds = new WrappingNeeds();
-    $this->assertEquals(14, $WrappingNeeds->ribbonNeeded("1x1x10"));
+    $wrappingNeeds = new WrappingNeeds();
+    $this->assertEquals(14, $wrappingNeeds->ribbonNeeded("1x1x10"));
   }
 
   /**
    * @expectedException Exception
    */
   public function testInvalidWrappingDimensions() {
-    $WrappingNeeds = new WrappingNeeds();
-    $WrappingNeeds->wrappingNeeded("23x4");
+    $wrappingNeeds = new WrappingNeeds();
+    $wrappingNeeds->wrappingNeeded("23x4");
   }
 
   /**
    * @expectedException Exception
    */
   public function testInvalidRibbonDimensions() {
-    $WrappingNeeds = new WrappingNeeds();
-    $WrappingNeeds->ribbonNeeded("23x4");
+    $wrappingNeeds = new WrappingNeeds();
+    $wrappingNeeds->ribbonNeeded("23x4");
   }
 }
